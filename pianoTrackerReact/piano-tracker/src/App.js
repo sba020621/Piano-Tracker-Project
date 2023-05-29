2f261e462708053ca94200f1452cda76c38365ba
sba190007 <102188621+sba190007@users.noreply.github.com>
2023-05-29 14:48:10 -0500

    Created react project and header

---
+++ pianoTrackerReact/piano-tracker/src/App.js
@@ -0,0 +1,22 @@
+import logo from './piano.jpeg';
+import './App.css';
+import { Grid } from '@mui/material';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>
+        <img src={logo} className='img'></img>
+        <h2>piano tracker</h2>
+      </div>
+
+      <Grid className="App-header">
+        
+
+      </Grid>
+
+    </div>
+  );
+}
+
+export default App;
